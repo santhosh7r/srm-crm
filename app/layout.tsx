77313c0a5624659e0d85592e5cc56afe69b5ade7
santhosh perumal <santhoshperumal7r@gmail.com>
2026-07-14 12:45:28 +0530

    bg changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export const viewport: Viewport = {
   userScalable: false,
   viewportFit: 'cover',
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#FFFFFF' },
+    { media: '(prefers-color-scheme: light)', color: '#FAF6EB' },
     { media: '(prefers-color-scheme: dark)', color: '#000000' },
   ],
 }
